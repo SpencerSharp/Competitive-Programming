e0b0429b9be16cdde086477dea072a93afbf9e36
Spencer Sharp <spencersharp@me.com>
2016-07-14 14:58:36 -0500

    Finished CANDY on Spoj

--- SPOJ/Classical Problems/CANDY.java
+++ SPOJ/Classical Problems/CANDY.java
@@ -7,23 +7,24 @@ public class CANDY
 {
     public static void main(String[] args) throws IOException
     {
-        BufferedReader f = new BufferedReader(new FileReader("/Users/spencersharp/Desktop/input.txt"));
+        //BufferedReader f = new BufferedReader(new FileReader("/Users/spencersharp/Desktop/input.txt"));
         
-        //BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter writer = new PrintWriter(new BufferedOutputStream(System.out));
         
         String output = ""; //Write all output to this string
 
         //Code here
-        ArrayList<Integer> candies = new ArrayList<Integer>();
         int size = Integer.parseInt(f.readLine());
+        
         while(size !=-1)
         {
-            int sum = 0; 
+            long[] candies = new long[size+1];
+            long sum = 0; 
             for(int i = 0; i < size; i++)
             {
-                int cur = Integer.parseInt(f.readLine());
-                candies.add(cur);
+                long cur = Long.parseLong(f.readLine());
+                candies[i]=cur;
                 sum += cur;
             }
             if(sum%size!=0)
@@ -32,16 +33,15 @@ public class CANDY
             }
             else
             {
-                int moves = 0;
-                if(getPair(sum/size)!=null)
+                long moves = 0;
+                for(int i = 0; i < candies.length; i++)
                 {
-                    
+                    if(candies[i]>(sum/size))
+                        moves+=(candies[i]-(sum/size));
                 }
-                else
-                {
-                    give(min,max);
-                }
+                writer.println(moves);
             }
+            size = Integer.parseInt(f.readLine());
         }
         
         
@@ -53,6 +53,18 @@ public class CANDY
         writer.println(output);*/
         writer.close();
         System.exit(0);
-        
+    }
+    
+    public static boolean isUniform(ArrayList<Integer> candies, int supposedToEqual)
+    {
+        for(int n : candies)
+            if(n!=supposedToEqual)
+                return false;
+        return true;
+    }
+    
+    public static String getPair(ArrayList<Integer> candies, int supposedToEqual)
+    {
+        return null;
     }
 }
